fda64a54845256bdb5ab9d24f0eae39ceabfb87e
eugenesvk <eugenesvk@users.noreply.github.com>
2022-07-02 15:19:11 +0700

    add mutable and for_each examples

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![allow(dead_code)]
+#![allow(unused_assignments)]
+#![allow(unused_attributes)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(unused_mut)]
+#![allow(unused_variables)]
+#![allow(unused_must_use)]
+
+mod p1; // Mutable
+
+fn main() {
+  p1::mutable();
+  p1::async_example();
+  p1::for_each();
+}
